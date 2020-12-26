9756a3ba61c61f301a7fec936fe32cc2b54b53f7
Pau <pauverasep@gmail.com>
2020-12-26 16:41:36 -0300

    inserción de código JS para elementos de menú

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+    /* Habilita el uso de tooltips */
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /* Esconde el menú desplegable una vez apretado un enlace */
+    $('.navbar-nav>li>a').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    /* Cambia la clase active del menú y vuelve a top al evento click */
+    $('.nav-link').click(function(){
+        $('.nav-item').removeClass('active');
+        $(this).parent().addClass('active');
+        window.scrollTo(0,0);
+    });
 })
